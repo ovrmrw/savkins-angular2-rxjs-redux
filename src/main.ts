cdf673eeb49cffe4697c1ef48a56d27df0394dd7
ovrmrw <t.ngc.0403@gmail.com>
2016-04-22 23:57:58 +0900

    Fixed

--- src/main.ts
+++ src/main.ts
@@ -139,15 +139,16 @@ function stateFn(initState: AppState, actions: Observable<Action>): Observable<A
 // -- DI config
 /*
   DI設定。Fluxの要。よくわからない。特にdispatcherがよくわからない。難しい。こんなことを考案したのは一体誰だ。
-  それでもここが理解できないとSavkin's Reduxは理解できない。特にstateFn関数にdispatcherがクロージャされることを把握できていないとどこで何が起きているのかわからないはずだ。
+  それでもここが理解できないとSavkin's Redux with RxJSは理解できない。調べるにあたっては英語力とググり能力が試される最大の難所と言っても良い。
   https://laco0416.github.io/post/platform-prividers-of-angular-2/ を参考にすると理解の助けになるかもしれない。
 */
 const INIT_STATE = new OpaqueToken("initState"); // "angular2 opaquetoken"でググる。
 const DISPATCHER = new OpaqueToken("dispatcher");
 const STATE = new OpaqueToken("state");
 
-// STATEの中でstateFn関数を呼んでいる。そしてDISPATCHERを依存関係に取り込んでいるので、
-// おそらく後述するdispatcher.next()が呼ばれたときにstateFnでクロージャされたactionsが変更されてSubscriptionが反応する仕組みなのだろう。(適当)
+// 最後の方に出てくるTodoAppコンポーネントのprovidersにセットしており、bootstrap()時に実体化される。(@laco0416 さんありがとう！)
+// stateFn()のインスタンスを生成するときにinitStateとdispatcherを引数にあてている(クロージャしている)ので、
+// 後述するdispatcher.next()がコールされたときにstateFn()内部へ伝播してObservableイベント(Subscription)を強制発火させている。(という個人的な解釈)
 const stateAndDispatcher = [
   provide(INIT_STATE, { useValue: { todos: [], visibilityFilter: 'SHOW_ALL' } }),
   provide(DISPATCHER, { useValue: new Subject<Action>(null) }), // ObservableではなくSubjectである。SubjectはObservableでもありObserverでもある。そこが重要。(適当)
